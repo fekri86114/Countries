75741141585fb83efddd716963350d97d91be5f5
fekri1386 <fekri.mohammadreza86@gmail.com>
2022-08-19 15:05:01 +0430

    created splash screen part, added viewBinding

--- app/src/main/java/com/example/countries/test.java
+++
@@ -1,4 +0,0 @@
-package com.example.countries;
-
-public class test {
-}
---
+++ app/src/main/java/com/example/countries/ui/activity/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.example.countries.ui.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.countries.databinding.ActivitySplashScreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity: AppCompatActivity() {
+    lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 1000 mil. == 1 sec.
+
+    }
+
+}
